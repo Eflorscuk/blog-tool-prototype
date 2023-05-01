c1034282f57e4340e53a0ea7a63a8297ffcb5d6d
Evandro Florscuk <evandro_ff@me.com>
2023-05-01 01:47:04 -0300

    App.js configured

---
+++ app.js
@@ -0,0 +1,34 @@
+const express = require('express')
+const Handlebars = require('handlebars')
+const { engine } = require('express-handlebars')
+const { allowInsecurePrototypeAccess } = require('@handlebars/allow-prototype-access')
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+
+const app = express()
+const port = 8084
+
+// Conf
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json())
+
+app.engine('handlebars',
+    engine({
+        defaultLayout: 'main',
+        handlebars: allowInsecurePrototypeAccess(Handlebars)
+    })
+)
+
+app.set('view engine', 'handlebars')
+app.set('views', './views')
+
+
+// Routes
+
+// Others
+
+try {
+    app.listen(port, _ => console.log('Successful'))
+} catch(e) {
+    console.log('Error ', e)
+}
