1a3401cec78a52ac9030a371377897d09f32408f
Evandro Florscuk <evandro_ff@me.com>
2023-05-02 01:27:55 -0300

    created form validation

--- routes/admin.js
+++ routes/admin.js
@@ -21,6 +21,24 @@ router.get("/categories/add", (req, res) => {
 })
 
 router.post("/categories/new", (req, res) => {
+    let errors = []
+
+    if(!req.body.name || typeof req.body.name == undefined || req.body.name == null) {
+        errors.push({ text: "Invalid Name" })
+    }
+
+    if(!req.body.slug || typeof req.body.slug == undefined || req.body.slug == null) {
+        errors.push({ text: "Invalid Slug"})
+    }
+
+    if(req.body.name.length < 2) {
+        errors.push({ text: "Category name is too small"})
+    }
+
+    if(errors.length > 0) {
+        res.render("admin/addCategories", { errors: errors })
+    }
+
     const newCategory = {
         name: req.body.name,
         slug: req.body.slug
