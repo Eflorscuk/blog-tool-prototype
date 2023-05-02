cb051ab4a9aac9e736cafd76f78d437c60eb94e4
Evandro Florscuk <evandro_ff@me.com>
2023-05-02 01:35:14 -0300

    Add sessions with flash and express session

--- app.js
+++ app.js
@@ -4,14 +4,29 @@ const { engine } = require('express-handlebars')
 const { allowInsecurePrototypeAccess } = require('@handlebars/allow-prototype-access')
 const bodyParser = require('body-parser')
 const path = require('path')
+const session = require('express-session')
+const flash = require('connect-flash')
 
 const admin = require("./routes/admin")
-const { conn } = require('./server')
+//const { conn } = require('./server')
 
 const app = express()
 const port = 8084
 
 // Conf
+app.use(session({
+    secret: "test",
+    resave: true,
+    saveUninitialized: true
+}))
+app.use(flash())
+
+app.use((req, res, next) => {
+    res.locals.success_msg = req.flash("success_msg")
+    res.locals.error_msg = req.flash("error_msg")
+    next()
+}) 
+
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json())
 
