269c04f043fcfdb8b2a456c225ad056a637ca29f
Evandro Florscuk <evandro_ff@me.com>
2023-05-03 00:13:43 -0300

    Created route and botton to edit category

--- routes/admin.js
+++ routes/admin.js
@@ -59,4 +59,52 @@ router.post("/categories/new", (req, res) => {
     })
 })
 
+router.get("/categories/edit/:id", (req, res) => {
+    Category.findOne({ _id: req.params.id }).then(category => {
+        res.render('admin/editCategory', { category: category})
+    }).catch(err => {
+        res.flash('error_msg', "This Id doesn't exist!")
+        res.redirect('/admin/categorias')
+    })
+})
+
+router.post("/categories/edit", (req, res) => {
+    let errors = []
+
+    if(!req.body.name || typeof req.body.name == undefined || req.body.name == null) {
+        errors.push({ text: "Invalid Name" })
+    }
+
+    if(!req.body.slug || typeof req.body.slug == undefined || req.body.slug == null) {
+        errors.push({ text: "Invalid Slug"})
+    }
+
+    if(req.body.name.length < 2) {
+        errors.push({ text: "Category name is too small"})
+    }
+
+    if(errors.length > 0) {
+        res.render("admin/addCategories", { errors: errors })
+        return
+    }
+
+    Category.findOne({ _id: req.body.id }).then(category => {
+        category.name = req.body.name
+        category.slug = req.body.slug
+
+        category.save().then(_ => {
+            req.flash("success_msg", "Category edited successfully")
+            res.redirect("/admin/categories")
+        }).catch(err => {
+            req.flash("error_msg", "Internal error saving category edit")
+            console.log("Error => ", err)
+            res.redirect("/admin/categories")
+        })
+    }).catch(err => {
+        req.flash("error_msg", "Category edit error")
+        console.log("Error Edit ===> ", err)
+        res.redirect("/admin/categories")
+    })
+})
+
 module.exports = router
